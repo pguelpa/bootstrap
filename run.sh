b3729a5fad4e94706b775d5aca0bec1bfbe8a4a8
Paul Guelpa <paul@shopkeep.com>
2016-04-16 23:43:26 -0400

    Initial bootstrap

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+printf "Checking for pip ... "
+which pip > /dev/null
+if [ $? -ne 0 ]; then
+  echo "No"
+  echo "Installing pip"
+  sudo easy_install pip
+else
+  echo "OK"
+fi
+
+printf "Checking for ansible ... "
+which ansible > /dev/null
+if [ $? -ne 0 ]; then
+  echo "No"
+  echo "Installing ansible"
+  sudo easy_install pip
+else
+  echo "OK"
+fi
+
+ansible-playbook -i hosts site.yml
