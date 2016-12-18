281eb230898030b6305e0461e58c1755bdbf39b1
Paul Guelpa <paul@shopkeep.com>
2016-12-18 12:30:29 -0500

    Updates from new computer

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # bootstrap
 Bootstrap my machine.
+
+## Usage
+
+1. Download this repository as a zip file
+1. Make sure your private keys are on the computer
+1. Run `./run.sh`
+1. Set the default shell `chsh -s /usr/local/bin/zsh`
--- run.sh
+++ run.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
+printf "Checking for XCode command line tools ... "
+xcode-select --print-path > /dev/null
+if [ $? -ne 0 ]; then
+  echo "No"
+  echo "Installing XCode command line tools, re-run this script once they are installed."
+  xcode-select --install
+  exit
+else
+  echo "OK"
+fi
+
 printf "Checking for pip ... "
 which pip > /dev/null
 if [ $? -ne 0 ]; then
   echo "No"
   echo "Installing pip"
-  sudo easy_install pip
+  easy_install --user pip
 else
   echo "OK"
+  export PATH=$PATH:$HOME/Library/Python/2.7/bin
 fi
 
 printf "Checking for ansible ... "
@@ -15,7 +27,17 @@ which ansible > /dev/null
 if [ $? -ne 0 ]; then
   echo "No"
   echo "Installing ansible"
-  sudo easy_install ansible
+  pip install --egg --user --upgrade ansible
+else
+  echo "OK"
+fi
+
+printf "Checking for Homebrew ... "
+which brew > /dev/null
+if [ $? -ne 0 ]; then
+  echo "No"
+  echo "Installing homebrew"
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
   echo "OK"
 fi
